720ce224aef8baff25d6254a63486077ed6af8d8
Mariusz <siewruk.mariusz92@gmail.com>
2018-11-06 22:09:23 +0000

    Updates

---
+++ precache-manifest.3567cd902952bceb35e44b2369794d47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Blog-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d73bc01816b02d26eeba",
+    "url": "/Blog-app/static/js/runtime~main.d73bc018.js"
+  },
+  {
+    "revision": "e1c8f2901396fbc7f766",
+    "url": "/Blog-app/static/js/main.e1c8f290.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/Blog-app/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "e1c8f2901396fbc7f766",
+    "url": "/Blog-app/static/css/main.6c2bfb9f.chunk.css"
+  },
+  {
+    "revision": "f0bd5491ae2fb57aa748d6bbaad8b558",
+    "url": "/Blog-app/index.html"
+  }
+];
