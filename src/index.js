47a686a0f68c5f9d5770cecb16660d3d86d6e342
Mariusz Siewruk <siewruk.mariusz92@gmail.com>
2018-11-07 10:21:56 +0000

    deleted app.js, added react-router

--- src/index.js
+++ src/index.js
@@ -1,19 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import App from './App';
+import { BrowserRouter, Route } from 'react-router-dom';
 import reducers from './reducers';
+
+import PostsIndex from './components/posts_index';
+
 
 serviceWorker.unregister();
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
+
+
 ReactDOM.render(
     <Provider store={createStoreWithMiddleware(reducers)}>
-        <App />
+        <BrowserRouter>
+            <div>
+                <Route path="/" component={PostsIndex} />
+            </div>
+        </BrowserRouter>
     </Provider>
     , document.getElementById('root'));
 
