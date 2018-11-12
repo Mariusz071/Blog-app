7ba745f88e2017e65fbcf2492e1690fd2c42ac1e
Mariusz Siewruk <siewruk.mariusz92@gmail.com>
2018-11-12 16:15:11 +0000

    added CssBaseline (normalize)

--- src/components/main_container.js
+++ src/components/main_container.js
@@ -6,18 +6,23 @@ import PostsIndex from "./posts_index";
 import HomePage from './home'
 import LoginPanel from "./login_panel";
 
+import CssBaseline from '@material-ui/core/CssBaseline';
+
 export default class MainContainer extends Component {
     render() {
         return (
-            <div className="home-page">
-                <Switch>
-                    <Route path="/posts/new" component={PostsNew} />
-                    <Route path="/posts/:id" component={PostDisplay} />
-                    <Route path="/posts" component={PostsIndex} />
-                    <Route path="/login" component={LoginPanel} />
-                    <Route path ="/" component={HomePage} />
-                </Switch>
-            </div>
+            <React.Fragment>
+                <CssBaseline/>
+                <div className="home-page">
+                    <Switch>
+                        <Route path="/posts/new" component={PostsNew} />
+                        <Route path="/posts/:id" component={PostDisplay} />
+                        <Route path="/posts" component={PostsIndex} />
+                        <Route path="/login" component={LoginPanel} />
+                        <Route path ="/" component={HomePage} />
+                    </Switch>
+                </div>
+            </React.Fragment>
 
         )
     }
