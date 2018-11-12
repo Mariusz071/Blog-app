33f2992f872b3736d97b58b6dff29743b0e5ce5c
Mariusz Siewruk <siewruk.mariusz92@gmail.com>
2018-11-12 14:26:02 +0000

    added styling to the footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,18 +5,22 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
-const styles = theme => ({
 
+const styles = theme => ({
     list: {
         marginBottom: theme.spacing.unit * 2,
     },
     appBar: {
         top: 'auto',
         bottom: 0,
+        background: "#FCDA02",
     },
     toolbar: {
         alignItems: 'center',
         justifyContent: 'space-between',
+    },
+    iconButton: {
+        color: "#797979"
     }
 });
 
@@ -28,10 +32,10 @@ function BottomAppBar(props) {
         <AppBar position="fixed" color="primary" className={classes.appBar}>
             <Toolbar className={classes.toolbar}>
                 <div>
-                    <IconButton color="inherit">
-                        <i className="fab fa-github-square"></i>
+                    <IconButton className={classes.iconButton}>
+                            <i className="fab fa-github-square"></i>
                     </IconButton>
-                    <IconButton color="inherit">
+                    <IconButton className={classes.iconButton}>
                         <i className="fab fa-linkedin"></i>
                     </IconButton>
                 </div>
