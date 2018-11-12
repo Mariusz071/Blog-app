e6f43f2654d78456ad484b02c6721095226335eb
Mariusz Siewruk <siewruk.mariusz92@gmail.com>
2018-11-12 14:27:36 +0000

    added styling for index page

--- src/components/home.js
+++ src/components/home.js
@@ -6,7 +6,7 @@ render () {
     return (
             <div className="home">
                 <Typography variant="r" className="home__heading">
-                    Welcome to BlogSimple App, feel free to play around after logging in.
+                    Welcome to  <span className="home__heading-span">BlogSimple</span> App, feel free to play around after logging in.
                 </Typography>
             </div>
         )
