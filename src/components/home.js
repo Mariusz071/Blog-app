db5a4bc57a53a1dcd89cf2b31db9df66b39015d9
Mariusz Siewruk <siewruk.mariusz92@gmail.com>
2018-11-14 15:09:37 +0000

    changed typography variant

--- src/components/home.js
+++ src/components/home.js
@@ -4,11 +4,9 @@ import Typography from '@material-ui/core/Typography';
 export default class HomePage extends Component {
 render () {
     return (
-            <div>
-                <Typography variant="r" className="home__heading">
-                    Welcome to  <span className="home__heading-span">BlogSimple</span> App, feel free to play around after logging in.
-                </Typography>
-            </div>
+            <Typography variant="inherit" className="home__heading">
+                Welcome to  <span className="home__heading-span">BlogSimple</span> App, feel free to play around after logging in.
+            </Typography>
         )
     }
 }
