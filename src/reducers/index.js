672162d4fe1c8a6f2fce2841e75b7bfbed69e74b
Mariusz Siewruk <siewruk.mariusz92@gmail.com>
2018-11-15 12:52:09 +0000

    wired up userKey reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,14 @@
 import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
 import PostsReducer from './reducer_posts';
+import userKeyReducer from './reducer_user-key';
+
+
 
 const rootReducer = combineReducers({
   posts: PostsReducer,
-  form: formReducer
+  form: formReducer,
+    key: userKeyReducer
 });
 
 export default rootReducer;
